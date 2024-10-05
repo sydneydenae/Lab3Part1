5824a3fb89c66b32ceaf5685ad4d101b91e74a85
Sydney Stokes <sydney.stokes@bison.howard.edu>
2024-10-05 03:27:57 +0000

    Update

--- pipes_processes3.c
+++ pipes_processes3.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 
 /**
  * Executes the command "cat scores | grep Lakers".  In this quick-and-dirty
@@ -28,45 +29,53 @@ int main(int argc, char **argv)
 
   // make a pipe (fds go in pipefd[0] and pipefd[1])
 
-  pipe(pipefd);
-  pipe(pipefdSort);
+  if (pipe(pipefd) == -1 || pipe(pipefdSort) == -1) {
+        perror("pipe");
+        return 1;
+    }
 
   pid = fork();
-
+  if (pid < 0) {
+        perror("fork");
+        return 1;
+  }
 
   if (pid == 0)
     {
       pid = fork();
+
+      if (pid < 0) {
+        perror("fork");
+        return 1;
+      }
 
       if (pid == 0){
         // child's child gets here and handles "sort"
         dup2(pipefdSort[0], 0);
         close(pipefdSort[1]);
          execvp("sort", sort_args); // Execute sort
-
+        return 1;
 
       } else {
         // child gets here and handles "grep Villanova"
-
         // replace standard input with input part of pipe
-
         dup2(pipefd[0], 0);
         dup2(pipefdSort[1], 1);
 
         // close unused hald of pipe
-
         close(pipefd[1]);
         close(pipefdSort[0]);
 
         // execute grep
         execvp("grep", grep_args);
+
+        return 1;
         
       }
     }
   else
     {
       // parent gets here and handles "cat scores"
-
       // replace standard output with output part of pipe
 
       dup2(pipefd[1], 1);
@@ -80,5 +89,11 @@ int main(int argc, char **argv)
       // execute cat
 
       execvp("cat", cat_args);
+      return 1;
     }
+
+    wait(NULL);
+    wait(NULL);
+
+    return 0;
 }
